5866f40dfe2c22f342062fae69b24fb622962d2d
Souleymane Diallo <souleymanetimbidiallo@gmail.com>
2021-02-08 19:51:47 +0100

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About Us | Hello City</title>
     </head>
     <body>
-       <h1>Hello from Rennes</h1>
+       <p>Built by &hearts; Soulman Dev</p>
        
-       <p>It's currently {{date('h:i A')}}</p>
+       <p><a href="/">Revernir à la page d'accueil</a></p>
 
         <footer>
             <p>&copy; copyright {{date('Y')}} &middot; <a href="/about-us">About us</a></p>
