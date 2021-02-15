1476bb66dadbb8b100d3960f2d06870a47c5c172
Souleymane Diallo <souleymanetimbidiallo@gmail.com>
2021-02-15 22:09:55 +0100

    Make use asset function helper to load images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
 @section('title', 'About Us | '.config('app.name'))
 
 @section('content')   
-    <img src="/images/avatar.png" style="height: 300px; width:300px" alt="Avatar">
+    <img src="{{asset('/images/avatar.png')}}" style="height: 300px; width:300px" alt="Avatar">
 
     <p>Built by &hearts; Soulman Dev</p>
         
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')   
-<img src="/images/rennes.png" style="height: 300px; width:300px" alt="Rennes Flag">
+<img src="{{asset('/images/rennes.png')}}" style="height: 300px; width:300px" alt="Rennes Flag">
 
 <h1>Hello from Rennes</h1>
 
